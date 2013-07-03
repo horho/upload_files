e328db5f42dd7c94b218046296b8c0901e748142
horho <horho@lapsang.(none)>
2013-07-03 10:36:12 +0200

    with handlers

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+
+function upload(pathname) {
+    console.log("Request for upload " + pathname + " page");
+}
+function start(pathname) {
+    console.log("Request for start " + pathname + " page");
+}
+
+exports.upload = upload;
+exports.start = start;
