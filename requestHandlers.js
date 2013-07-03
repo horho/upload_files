fb121b9fd085701e2d8b0edbf6286fc41ef09bde
horho <horho@lapsang.(none)>
2013-07-03 10:50:07 +0200

    bad non-blocking requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,18 @@
+var exec = require("child_process").exec;
+
+function start(pathname) {
+	console.log("Request for start " + pathname + " page");
+	var content = "empty";
+
+	exec("ls -lah", function(error, stdout, stderr) {
+		content = stdout;
+		});
+	return content;
+}
 
 function upload(pathname) {
     console.log("Request for upload " + pathname + " page");
-}
-function start(pathname) {
-    console.log("Request for start " + pathname + " page");
+		return "Hello Upload";
 }
 
 exports.upload = upload;
--- server.js
+++ server.js
@@ -6,9 +6,8 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 
-		route(handle, pathname);
+		var message = route(handle, pathname);
 
-		var message = "Hello World!";
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(message);
 		response.end();
